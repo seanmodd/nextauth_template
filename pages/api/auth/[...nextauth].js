ea1e7738607c99758dde13145046099da07a1329
Sean <sean@vestaridefair.com>
2021-08-22 20:40:48 -0700

    erased _app unnecessary comments and grayed out code

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -28,23 +28,4 @@ export default NextAuth({
     },
   },
 });
-// database: process.env.DB_URL,
-// session: {
-//   jwt: true,
-// },
-// jwt: {
-//   secret: 'asdcvbtjhm',
-// },
-// callbacks: {
-//   async jwt(token, user) {
-//     if (user) {
-//       token.id = user.id;
-//     }
-//     return token;
-//   },
-//   async session(session, token) {
-//     session.user.id = token.id;
-//     return session;
-//   },
-// },
-// });
+
